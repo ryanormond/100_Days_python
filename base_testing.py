94c233a9f68c4b901df57543eeafdc7bc891d21f
Ryan Ormond <zarmortiz19@live.com>
2024-07-25 15:43:10 +0700

    base file for playground code

---
+++ base_testing.py
@@ -0,0 +1,7 @@
+print(len("Hello"))
+
+#takes and input and returns the length of that input
+# as an integer into the variable
+num_char = len(input("What is your name?"))
+#prints the length of the input as integer
+print(num_char) 
